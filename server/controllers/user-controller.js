30b833dcbebf278f2d09b8bb0c162d09c6c0e773
nipunaupeksha <nipuna.upeksha@gmail.com>
2023-01-02 16:00:01 +0530

    initial routes testing

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -7,6 +7,8 @@ const ERROR_CONNECTING = "Error conneting to the server!"
 const ERROR_SENDING_EMAIL = "Error sending mail to the given mail!"
 const ERROR_INVALID_EMAIL = "Invalid email address!"
 const ERROR_INVALID_CREDENTIALS = "Invalid credentials!"
+const ERROR_USERNAME_ALREADY_EXISTS = "Username already exists!"
+const ERROR_EMAIL_ALREADY_EXISTS = "email already exists!"
 
 exports.signup = function (req, res, next) {
     let email = req.body.email;
@@ -14,14 +16,37 @@ exports.signup = function (req, res, next) {
     let username = req.body.username;
     let fullname = req.body.fullname;
 
-    let _query = "INSERT INTO user(username, fullname, email, password) values (?,?,?,?)";
-    dbConfig.query(_query, [username, fullname, email, password], (err, rows) => {
+    let _query = "SELECT * FROM user WHERE username = ?";
+    dbConfig.query(_query, [username], (err, rows) => {
         if (err) {
             console.log(ERROR_CONNECTING);
-            console.log(err);
-            return res.status(500).send({ success: false, message: ERROR_CONNECTING })
+            return res.status(500).send({ success: false, message: ERROR_CONNECTING });
         } else {
-            res.status(201).send({ success: true, data: { email: email, password: password, username: username, message: "Account successfully created!" } })
+            if (rows != null && rows.length!=0) {
+                return res.status(401).send({ success: false, message: ERROR_USERNAME_ALREADY_EXISTS });
+            } else {
+                let _query_1 = "SELECT * FROM user WHERE email = ?";
+                dbConfig.query(_query_1, [email], (err, rows) => {
+                    if (err) {
+                        console.log(ERROR_CONNECTING);
+                        return res.status(500).send({ success: false, message: ERROR_CONNECTING });
+                    } else {
+                        if (rows != null && rows.length!=0) {
+                            return res.status(401).send({ success: false, message: ERROR_EMAIL_ALREADY_EXISTS })
+                        } else {
+                            let _query_2 = "INSERT INTO user(username, fullname, email, password) VALUES (?,?,?,?)";
+                            dbConfig.query(_query_2, [username, fullname, email, password], (err, rows) => {
+                                if (err) {
+                                    console.log(ERROR_CONNECTING);
+                                    return res.status(500).send({ success: false, message: ERROR_CONNECTING })
+                                } else {
+                                    return res.status(201).send({ success: true, data: { email: email, password: password, username: username, message: "Account successfully created!" } })
+                                }
+                            });
+                        }
+                    }
+                });
+            }
         }
     });
 };
@@ -30,25 +55,27 @@ exports.signin = function (req, res, next) {
     let username = req.body.username;
     let password = req.body.password;
 
-    let _query = "SELECT * from user where username = ? and password=?";
+    let _query = "SELECT * FROM user WHERE username = ? AND password = ?";
     dbConfig.query(_query, [username, password], (err, rows) => {
         if (err) {
             console.log(ERROR_CONNECTING)
-            console.log(err);
             return res.status(500).send({ success: false, message: ERROR_CONNECTING })
         } else {
-            try{
-                if(rows!=null){
-                    if(password==rows[0]['password']){
-                        return res.status(200).send({succes:true, message: "Login successful!"})
+            try {
+                if (rows != null && rows.length!=0) {
+                    if (password === rows[0]['password']) {
+                        return res.status(200).send({ success: true, message: "Login successful!" })
+                    } else {
+                        console.log(ERROR_INVALID_CREDENTIALS);
+                        return res.status(401).send({ success: false, message: ERROR_INVALID_CREDENTIALS })
                     }
-                }else{
+                } else {
                     console.log(ERROR_INVALID_CREDENTIALS);
-                    return res.status(401).send({success: false, message: ERROR_INVALID_CREDENTIALS})
+                    return res.status(401).send({ success: false, message: ERROR_INVALID_CREDENTIALS })
                 }
-            }catch(e){
+            } catch (e) {
                 console.log(e);
-                return res.status(401).send({success: false, message: ERROR_INVALID_CREDENTIALS})
+                return res.status(401).send({ success: false, message: ERROR_INVALID_CREDENTIALS })
             }
         }
     });
@@ -57,7 +84,7 @@ exports.signin = function (req, res, next) {
 exports.forgetPassword = function (req, res, next) {
     let email = req.body.email;
 
-    let _query = "SELECT * from user where email = ?";
+    let _query = "SELECT * FROM user WHERE email = ?";
     dbConfig.query(_query, [email], (err, rows) => {
         if (err) {
             console.log(ERROR_CONNECTING);
@@ -79,18 +106,17 @@ exports.forgetPassword = function (req, res, next) {
                     subject: "Reset password of your Holux account.",
                     text: "Your six digit token: " + randomNo
                 };
-                let _query = "UPDATE user set forgetPasswordNo = ? where email = ?";
+                let _query = "UPDATE user SET forgetPasswordNo = ? WHERE email = ?";
                 dbConfig.query(_query, [randomNo, email], (err, rows) => {
                     if (err) {
                         console.log(ERROR_CONNECTING);
-                        console.log(err);
                         return res.status(500).send({ success: false, message: ERROR_CONNECTING });
                     } else {
                         transporter.sendMail(mailOptions, function (error, info) {
                             if (error) {
                                 return res.status(404).send({ success: false, message: ERROR_SENDING_EMAIL })
                             } else {
-                                res.status(200).send({ succes: true, data: { email: email, message: "Successfully send the mail!" } })
+                                return res.status(200).send({ success: true, data: { email: email, message: "Successfully send the mail!" } })
                             }
                         })
                     }
@@ -107,14 +133,13 @@ exports.changePassword = function (req, res, next) {
     let username = req.body.username;
     let password = req.body.password;
 
-    let _query = "UPDATE user set password=? where username=?";
+    let _query = "UPDATE user SET password=? WHERE username=?";
     dbConfig.quert(_query, [password, username], (err, rows) => {
         if (err) {
             console.log(ERROR_CONNECTING);
-            console.log(err);
-            return res.status(500).send({ succes: false, message: ERROR_CONNECTING })
+            return res.status(500).send({ success: false, message: ERROR_CONNECTING })
         } else {
-            res.status(200).send({ succes: true, data: { message: "Password succesfully changed!", username: username, password: password } });
+            return res.status(200).send({ success: true, data: { message: "Password succesfully changed!", username: username, password: password } });
         }
     })
 }
