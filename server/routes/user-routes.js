1bf85444f30adbdc5543be1708017cb5d259e57d
nipunaupeksha <nipuna.upeksha@gmail.com>
2023-01-10 00:06:20 +0530

    route changes

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user-controller");
 
-router.post("/signup", userController.signup);
-router.post("/signin", userController.signin);
-router.post("/forgetpassword", userController.forgetPassword);
-router.post('/forgetpassword/token',userController.tokenValidation); 
-router.post("/forgetpassword/changepassword", userController.changePassword);
+router.post("/sign-up", userController.signup);
+router.post("/sign-in", userController.signin);
+router.post("/forget-password", userController.forgetPassword);
+router.post('/forget-password/token',userController.tokenValidation); 
+router.post("/forget-password/change-password", userController.changePassword);
 
 module.exports = router;
